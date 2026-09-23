9e1a320c346a051e0b47512804e4387fc91db35f
Micido <titouan.poli@free.fr>
2026-09-23 21:32:37 +0200

    lualine plugin v1

---
+++ lua/plugins/ui/lualine.lua
@@ -0,0 +1,11 @@
+vim.pack.add({
+	{ src = "https://github.com/nvim-lualine/lualine.nvim", name = "lualine" },
+})
+
+requires("lualine").setup({
+	options = {
+		icons_enabled = true,
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+	},
+})
